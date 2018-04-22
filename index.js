e3a6edea92aef5f524357f75a7d3802f4c69e265
Jan Stránský <jan.stransky@arnal.cz>
2018-04-22 03:38:15 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const fs = require('fs')
+const dirs = ['models', 'examples/models']
+var output = {}
+dirs.forEach(d => {
+  let target = null
+  let splt = d.split("/")
+  if (splt.length > 1) {
+    output[splt[0]] = {}
+    target = output[splt[0]][splt[1]] = {}
+  } else {
+    target = output[d] = {}
+  }
+  fs.readdirSync(path.join(__dirname,d)).forEach(f => {
+    let pp = path.parse(f)
+    if (pp.ext !== '.json') {
+      return
+    }
+    target[pp.name] = require(path.join(__dirname,d,f))
+  })
+})
+module.exports = output
