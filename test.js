6194afbe6ce80bc7c39416f9d2b114f6c0dad445
Jan Stránský <jan.stransky@arnal.cz>
2018-07-23 05:27:22 +0200

    Fix schema & add Standard linter

--- test.js
+++ test.js
@@ -1,82 +1,58 @@
-const request = require('request')
-//const djv = require('djv')
+/* global describe, it */
+
 const Ajv = require('ajv')
-const assert = require('assert')
 
 const CryptoSchema = require('.')
 const MetaSchema = require('ajv/lib/refs/json-schema-draft-06.json')
 
 var cats = [ 'models' ]
 
-function walk(root, example = false) {
+function checkSchema (c, ci, ki, example) {
+  let env = new Ajv()
+  env.addMetaSchema(MetaSchema)
+  env.addSchema(CryptoSchema.models.core, CryptoSchema.models.core.$id)
+
+  let testSchema = null
+
+  if (example) {
+    Object.keys(CryptoSchema[ci]).forEach(ccp => {
+      if (ccp === 'core') {
+        return null
+      }
+      let s = Object.assign({}, CryptoSchema[ci][ccp])
+      // s.additionalProperties = false
+      env.addSchema(s, s.$id)
+    })
+    testSchema = CryptoSchema[ci][ki]
+  } else {
+    testSchema = MetaSchema
+  }
+
+  let res = env.validate(testSchema, c[ki])
+  if (!res) {
+    throw new Error('Validation error: ' + JSON.stringify(env.errors))
+  }
+}
+
+function walk (root, example = false) {
   cats.forEach((ci) => {
     let c = root[ci]
-    describe(ci, function() {
+    describe(ci, function () {
       Object.keys(c).forEach((ki) => {
-        it(ki, function() {
-
-          let env = new Ajv()
-          env.addMetaSchema(MetaSchema)
-
-          /*let env = new djv({
-            version: 'draft-06',
-            //errorHandler: () => { "console.log(this.data); r" }
-            errorHandler(errorType) {
-              const path = this.data.toString().replace(/^data/, '')
-              const dataPath = path
-                .replace(/\['([^']+)'\]/ig, '.$1')
-                .replace(/\[(i[0-9]*)\]/ig, '[\'+$1+\']');
-              const schemaPath = `#${
-                path
-                  .replace(/\[i([0-9]*)\]/ig, '/items')
-                  .replace(/\['([^']+)'\]/ig, '/properties/$1')
-              }/${errorType}`
-
-              return `return {
-                keyword: '${errorType}',
-                dataPath: '${dataPath}',
-                schemaPath: '${schemaPath}'
-              };`
-            }
-
-          })*/
-          let schemaId = null
-
-          if (example) {
-
-            env.addSchema(CryptoSchema.models.core, CryptoSchema.models.core.$id)
-            env.addSchema(CryptoSchema.models.asset, CryptoSchema.models.asset.$id)
-            env.addSchema(CryptoSchema.models.network, CryptoSchema.models.network.$id)
-
-            Object.keys(CryptoSchema[ci]).forEach(ccp => {
-              if (ccp === 'core') {
-                return null
-              }
-              let s = Object.assign({}, CryptoSchema[ci][ccp])
-              //s.additionalProperties = false
-              env.addSchema(s, s.$id)
-            })
-            schemaId = CryptoSchema[ci][ki].$id
-
-          }
-
-          let res = env.validate(c[ki], schemaId)
-          if (!res) {
-            throw new Error('Validation error: ' + JSON.stringify(env.errors))
-          }
+        it(ki, function () {
+          checkSchema(c, ci, ki, example)
         })
       })
     })
   })
 }
 
-describe('opencrypto-schema tests', function() {
-
-  describe('testing schemas', function() {
+describe('opencrypto-schema tests', function () {
+  describe('testing schemas', function () {
     walk(CryptoSchema)
   })
 
-  describe('examples', function() {
+  describe('examples', function () {
     walk(CryptoSchema.examples, true)
   })
 })
